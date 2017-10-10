6887aa2a9c4f17749dfa916b6921753708ece6fb
j-christoffersen <jacksontchristo@gmail.com>
2017-10-09 20:24:09 -0700

    Progress 10/9

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,25 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var i = 0;
+  var parseHelper = function() {
+    if (json[i] === '[') {
+      var arr = [];
+      i++;
+      while (json[i] !== ']') {
+        arr.push(parseHelper());
+      }
+    } else {
+      var end = json.slice(i).indexOf(',');
+
+      var numberString = json.slice(i, end);
+      i += end;
+      return Number(numberString);
+    }
+  };
 };
+
+
+// [11,2]
+
+//
