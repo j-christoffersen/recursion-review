4b43eb3f335dcfaf190084c8d980a3c2e9dd1ac6
j-christoffersen <jacksontchristo@gmail.com>
2017-10-10 21:33:49 -0700

    Somewhat finish parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,21 +4,56 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   var i = 0;
+
   var parseHelper = function() {
     if (json[i] === '[') {
+      //Array
       var arr = [];
-      i++;
-      while (json[i] !== ']') {
+      do {
+        i++;
+        if (json[i] === ' ') { i++; }
         arr.push(parseHelper());
-      }
+      } while (json[i] !== ']');
+      i++;
+      return arr;
+    } else if (json[i] === '{') {
+      //Object
+      var obj = {};
+      do {
+        i++;
+        if (json[i] === ' ') { i++; }
+        var key = parseHelper();
+        i++;
+        if (json[i] === ' ') { i++; }
+        obj[key] = parseHelper();
+      } while (json[i] !== '}');
+      i++;
+      return obj;
     } else {
-      var end = json.slice(i).indexOf(',');
-
-      var numberString = json.slice(i, end);
-      i += end;
-      return Number(numberString);
+      var s = '';
+      
+      
+      while (i !== json.length && json[i] !== ',' && json[i] !== ']' && json[i] !== '}' && json[i] !== ':') {
+        s += json[i];
+        i++;
+      }
+      
+      if (s === 'true') {
+        return true; 
+      } else if (s === 'false') {
+        return false;         
+      } else if (s === 'null') {
+        return null;         
+      } else if (s[0] === '\"') {
+        return s.slice(1, s.length - 1 ); 
+      } else {
+        return parseInt(s); 
+      }
+      
     }
   };
+
+  return parseHelper();
 };
 
 
